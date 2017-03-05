2fc1830617551b5337a575c0382385724f3de0cc
Marco Cedaro <cedmax@users.noreply.github.com>
2017-03-05 08:54:56 +0000

    better credits

--- src/components/pages/template.js
+++ src/components/pages/template.js
@@ -11,8 +11,10 @@ export default function Template(props) {
       <div className={ style.main }>{ props.children }</div>
       <footer className={ style.footer }>
         Star Wars is a Disney intellectual property.<br/>
-        Star Wars icons designed by <a href="https://dribbble.com/shots/2383021-Star-Wars-Icons-Freebie">Justas Galaburda</a>.<br/>
-        Star svg are from <a href="http://getemoji.com/">GetEmoji</a>.
+        Star Wars icons by <a href="https://dribbble.com/shots/2383021-Star-Wars-Icons-Freebie">Justas Galaburda</a>,
+        font by <a href="http://www.dafont.com/shyfonts.d11">Shyfonts</a>,
+        <br/> stars by <a href="http://getemoji.com/">GetEmoji</a> & 
+        made with <span className={style.heart}>♥</span> by <a href="https://cedmax.com/">cedmax</a>.
       </footer>
     </div>
   );
