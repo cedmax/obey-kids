51b3c257e7b5b85e9d8a1dffa958654d5bde5776
Marco Cedaro <cedmax@users.noreply.github.com>
2017-03-10 23:18:43 +0000

    added an observable day to the mobx store

--- src/store/mobx.js
+++ src/store/mobx.js
@@ -3,6 +3,7 @@ import { observable, action, extendObservable } from 'mobx';
 class Store {
   @observable user = null;
   @observable kids = {};
+  @observable day = '';
 
   @action.bound setUser(user) {
     this.user = user;
@@ -12,6 +13,10 @@ class Store {
     name && extendObservable(this.kids, {
       [name]: stars
     });
+  }
+
+  @action.bound setDay(day) {
+    this.day = day;
   }
 }
 
