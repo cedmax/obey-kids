b055a5e1586924e3b6ada266731f30779d2c02a2
Marco Cedaro <cedmax@users.noreply.github.com>
2017-03-11 02:20:53 +0000

    added a fill the gap up until now

--- src/helpers/get-dates.js
+++ src/helpers/get-dates.js
@@ -1,24 +1,39 @@
 import constants from 'store/constants';
 import moment from 'moment';
 
-export default {
-  prev(date) {
-    return moment(date, constants.DATE_FORMAT)
-      .subtract(1, 'days')
-      .format(constants.DATE_FORMAT);
-  },
+function today() {
+  return moment().format(constants.DATE_FORMAT)
+}
 
-  next(date) {
-    return moment(date, constants.DATE_FORMAT)
-      .add(1, 'days')
-      .format(constants.DATE_FORMAT);
-  },
+function prev(date) {
+  return moment(date, constants.DATE_FORMAT)
+    .subtract(1, 'days')
+    .format(constants.DATE_FORMAT);
+}
 
-  today() {
-    return moment().format(constants.DATE_FORMAT);
-  },
+function next(date) {
+  return moment(date, constants.DATE_FORMAT)
+    .add(1, 'days')
+    .format(constants.DATE_FORMAT);
+}
 
-  pretty(date) {
-    return moment(date, constants.DATE_FORMAT).format('DD/MM/YYYY');
+function pretty(date) {
+  return moment(date, constants.DATE_FORMAT).format('DD/MM/YYYY');
+}
+
+function upUntilNow(date) {
+  const results = [];
+  const refDate = today();
+  while (date !== refDate) {
+    date = next(date);
+    results.push(date);
   }
+  return results;
+}
+export default {
+  today,
+  prev,
+  next,
+  pretty,
+  upUntilNow
 };
