7d19cb37c70cd7cacc59e045760ca0647e9d7218
Marco Cedaro <cedmax@users.noreply.github.com>
2017-03-11 09:52:45 +0000

    moved the kids to be a map

--- src/store/mobx.js
+++ src/store/mobx.js
@@ -1,22 +1,27 @@
-import { observable, action, extendObservable } from 'mobx';
+import { observable, action, computed } from 'mobx';
 
 class Store {
   @observable user = null;
-  @observable kids = {};
   @observable day = '';
   @observable direction = {
     prev: false,
     next: false
   };
 
+  @computed get kids() {
+    return this.kidsMap.toJS();
+  }
+
+  constructor() {
+    this.kidsMap = observable.map({});
+  }
+  
   @action.bound setUser(user) {
     this.user = user;
   }
 
   @action.bound setKid(name, stars) {
-    name && extendObservable(this.kids, {
-      [name]: stars
-    });
+    name && this.kidsMap.set(name, stars);
   }
 
   @action.bound enableNav(direction, value) {
