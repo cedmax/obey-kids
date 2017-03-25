f43c83776463bbe1910b474ca91be6d52ef0dcfe
Marco Cedaro <cedmax@users.noreply.github.com>
2017-03-25 15:33:45 +0000

    removed unused parameters in dev server

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.config')
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(config.port, config.host, function (err, result) {
+}).listen(config.port, config.host, (err) => {
   if (err) {
-    console.log(err);
+    console.log(err)
   }
 
-  console.log(`Listening at http://${config.host}:${config.port}`);
-});
+  console.log(`Listening at http://${config.host}:${config.port}`)
+})
